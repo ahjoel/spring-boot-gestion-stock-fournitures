621c5f2fe690bd5e0e9a6872a6b8b2b63b8f6a0d
ahjoel <aholoujoel5@gmail.com>
2023-07-31 19:01:30 +0000

    api employe

--- src/main/java/com/gestock/fourniture/presentation/EmployeController.java
+++ src/main/java/com/gestock/fourniture/presentation/EmployeController.java
@@ -6,58 +6,27 @@ import com.gestock.fourniture.service.EmployeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api")
 public class EmployeController {
 
     @Autowired
     private EmployeService employeService;
 
-    @GetMapping("/lemployes")
-    public String listeEmploye(Model model) {
-        var employes = employeService.listEmployes();
-        model.addAttribute("employes",employes);
-        return "admin/employe/listeEmploye";
+    @RequestMapping(path="employes", method = RequestMethod.GET)
+    public List<EmployeDto> listeEmploye() {
+        return employeService.listEmployes();
     }
 
-    @GetMapping("/formemploye")
-    public String formulaireEmploye(Model model) {
-        var employeDTO = EmployeDto.builder()
-                .codeEmp("")
-                .nomEmp("")
-                .prenomEmp("")
-                .serviceEmp("")
-                .etat("ACTIVE")
-                .build();
-
-        model.addAttribute("employeDTO", employeDTO);
-
-        return "admin/employe/formEmploye";
-    }
-
-    @PostMapping("/enregistreremploye")
-    public String enregistrerEmploye(@ModelAttribute EmployeDto employeDto, RedirectAttributes redirectAttributes){
-
-        employeService.ajouterEmploye(employeDto);
-
-        redirectAttributes.addFlashAttribute("message", "Employé crée avec succes !");
-
-        return "redirect:lemployes";
-    }
-
-    @GetMapping("employe/{code}/edit")
-    public String showEditForm(@PathVariable("code") String codeEmp, Model model) {
-
-        EmployeDto employeDtoFound = getEmployeDtoCodeEmpExist(codeEmp);
-
-        model.addAttribute("employeDto", employeDtoFound);
-
-        return "admin/employe/formEditEmploye";
+    @RequestMapping(value="employe/addemp", method = RequestMethod.POST)
+    public Long enregistrerEmploye(@RequestBody EmployeDto employeDto){
+        return employeService.ajouterEmploye(employeDto);
     }
 
     private EmployeDto getEmployeDtoCodeEmpExist(String codeEmp) {
@@ -70,29 +39,24 @@ public class EmployeController {
         return employeDtoFound;
     }
 
-    @PostMapping("employe/{code}/modify")
-    public String processEditForm(@PathVariable("code") String codeEmp,
-                                  @ModelAttribute("employeDto") EmployeDto employeDto,
-                                  RedirectAttributes redirectAttributes) {
-        EmployeDto employeDtoFound = getEmployeDtoCodeEmpExist(codeEmp);
-
-        if (employeDtoFound == null) {
-            redirectAttributes.addFlashAttribute("message", "Code 257 : le code employe que vous voulez modifier n'existe pas");
-            return "admin/employe/formEditEmploye";
-        }
-
-        try {
-            employeService.modifierEmploye(employeDto);
-            redirectAttributes.addFlashAttribute("message", "Modification de l'employé " + codeEmp + " effectuée");
-            return "redirect:/lemployes";
-        } catch (RuntimeException e) {
-            String errorMessage = "Une erreur s'est produite lors de la modification de l'employe - " + e.getMessage();
-            redirectAttributes.addFlashAttribute("message", errorMessage);
-            return "admin/employe/formEditEmploye";
-        }
+    @RequestMapping(value="employe/{code}", method = RequestMethod.GET)
+    public EmployeDto afficherEmploye(@PathVariable("code") String codeEmp) {
+        return getEmployeDtoCodeEmpExist(codeEmp);
     }
 
+    @RequestMapping(value="/modifieremp", method=RequestMethod.PUT)
+    public boolean modifierEmploye(@RequestBody EmployeDto employeDto) {
+        return employeService.modifierEmploye(employeDto);
+    }
+
+    //@RequestMapping(value="/delprod/{id}", method=RequestMethod.DELETE)
+    //public void deleteProduit(@PathVariable("id") Long id) {
+    //    produitService.deleteProduitById(id);
+    //}
+
+
     // Traitement de la suppression de l'employe
+    @RequestMapping(value="/suppemp/{id}", method=RequestMethod.DELETE)
     @PostMapping("employe/delete/{code}")
     public String deleteEmploye(@PathVariable("code") String codeEmp, RedirectAttributes redirectAttributes) {
         try {
